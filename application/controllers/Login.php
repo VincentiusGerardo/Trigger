4e45c0d50c8bae5934caac8f3a4ffabf4eca26ef
Vincentius Gerardo <vincentiusgerardo11@gmail.com>
2019-02-01 16:49:48 +0700

    comit

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,5 +1,5 @@
 <?php
-
+    defined('BASEPATH') OR exit('No direct script access allowed');
     class Login extends CI_Controller{
         public function __construct(){
             parent::__construct();
@@ -14,6 +14,6 @@
         }
 
         public function doLogOut(){
-            
+
         }
     }
